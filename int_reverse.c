ea579bccff01934a241ef8d92133e706728fa136
胡枫 <yameng.sunyameng@koubei.com>
2017-08-24 16:34:17 +0800

    add int_reverse

---
+++ int_reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int reverse(int x) {
+    long long res = 0;
+    while(x) {
+        res = res * 10  + x%10;
+        x /= 10;
+    }
+    return (res < INT_MIN || res > INT_MAX) ? 0: res;
+}
+
+int main(void) {
+    int num = 321;
+    printf("result=%d\n", reverse(num));
+    return 0;
+}
