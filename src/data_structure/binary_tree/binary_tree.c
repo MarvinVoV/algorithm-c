c86757114ea8504524f017c55b4c9c154a5392f1
MarvinVoV <marvin_vov@outlook.com>
2021-02-11 16:19:23 +0800

    fix

--- src/data_structure/binary_tree/binary_tree.c
+++ src/data_structure/binary_tree/binary_tree.c
@@ -12,17 +12,100 @@ struct BTreeNode {
 
 typedef struct BTreeNode *Node;
 
-Node create(int *array, int len) {
-    printf("%d\n", array[5]);
-    Node root = (Node)malloc(sizeof(struct BTreeNode));
-
-
+/**
+ * Create Node
+ * @param data
+ * @return
+ */
+Node newNode(int data) {
+    Node node = (Node) malloc(sizeof(struct BTreeNode));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
 }
 
-int main(int argc, char *arv[]) {
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    create(array, 1);
+/**
+ * Construct a complement binary tree from a given array.
+ * Input  :  arr[] = {1, 2, 3, 4, 5, 6}
+ *
+ * Output:
+ *
+ *                  1
+ *                / \
+ *               2   3
+ *              / \ /
+ *             4  5 6
+ *
+ * If parent node is at index i in the array then left child of that node
+ * is at index (2*i+1) and right child is at index (2*i + 2) in the array.
+ *
+ * @param arr
+ * @param root
+ * @param i
+ * @param n
+ * @return
+ */
+Node insertLevelOfOrder(int arr[], Node root, int i, int n) {
+    // Base case for recursion
+    if (i < n) {
+        Node temp = newNode(arr[i]);
+        root = temp;
 
+        // insert left child
+        root->left = insertLevelOfOrder(arr, root->left, 2 * i + 1, n);
+
+        // insert right child
+        root->right = insertLevelOfOrder(arr, root->right, 2 * i + 2, n);
+    }
+    return root;
+}
+
+/**
+ * Pre-Order
+ * @param root
+ */
+void preOrder(Node root) {
+    if (NULL != root) {
+        printf("\n %d", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+/**
+ * In-Order
+ * @param root
+ */
+void inOrder(Node root) {
+    if (NULL != root) {
+        inOrder(root->left);
+        printf("\n %d", root->data);
+        inOrder(root->right);
+    }
+}
+
+/**
+ * Post-Order
+ * @param root
+ */
+void postOrder(Node root) {
+    if (NULL != root) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("\n %d", root->data);
+    }
+}
+
+int main(void) {
+    int array[] = {1, 2, 3, 4, 5, 6};
+    int len = sizeof(array) / sizeof(array[0]);
+    Node root = insertLevelOfOrder(array, NULL, 0, len);
+
+//    preOrder(root);
+//    inOrder(root);
+    postOrder(root);
+    return 0;
 }
 
 
